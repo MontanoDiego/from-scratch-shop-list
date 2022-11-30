c0dbd0179b78d337e7cb6d2aed033250f125ed40
MontanoDiego <diego.e2.montano@gmail.com>
2022-11-30 00:06:36 -0800

    delete button now deletes all list and renders

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* Imports */
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
-import { buyItem, checkAuth, createItem, fetchAllItems } from './fetch-utils.js';
+import { buyItem, checkAuth, createItem, deleteAllItems, fetchAllItems } from './fetch-utils.js';
 import { renderItem } from './render-utils.js';
 
 /* Get DOM Elements */
@@ -52,4 +52,9 @@ itemForm.addEventListener('submit', async (i) => {
     } else {
         shopList.textContent = 'Error adding item';
     }
+});
+
+deleteBtn.addEventListener('click', async () => {
+    await deleteAllItems();
+    await displayItems();
 });
